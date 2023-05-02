a2d06e7feef05d8c4c95845cdc79fe917c8b0a7a
ragan <raganalemu3@gmail.com>
2023-05-02 12:49:54 +0300

    Complete project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: "./",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
